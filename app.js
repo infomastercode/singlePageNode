5f026651554d3e10c9a356cc15db6afa1b759482
myProject <infomastercode@gmail.com>
2018-04-22 18:41:45 +0700

    add test

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-let fs = require('fs');
-
-app.use(function(a,b,x){
-	console.log('x');
-	x();
-});
+const fs = require('fs');
 
 app.get('/', function(req, res){
-	 res.sendFile(__dirname + '/index3.html');
+	 res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/product', function(req, res){
-	 res.send('hello');
-});
-
-app.get('/hi/:s', function(req, res){
-	 res.send('hello');
-});
-
-app.get('/ex4_1.html', function(req, res){
-	let y = fs.readFileSync('ex4_1.html', 'utf-8')
-	 res.send(y);
-});
 
 app.get('/page/:page', function(req, res){
 	res.end(fs.readFileSync(req.params.page+'.html', 'utf-8'));
